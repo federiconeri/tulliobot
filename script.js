d351ff028e152864fd2967e021c1d0b0ea427b59
federiconeri <federiconr@gmail.com>
2016-05-14 19:04:45 -0700

    Update script.js
    Update welcome message

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Ciao! My name is Tullio 👋 Let me introduce you to my creator, Federico? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
